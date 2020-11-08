bcacec2679d00fa69c4a1699fd02027afbe32ff1
Valentins Fedotovs <vtrader@inbox.lv>
2020-11-08 18:12:45 +0000

    Added format_date file and function decription

---
+++ format_data.py
@@ -0,0 +1,17 @@
+import re
+
+
+def oneline_report(source_file: str, dest_file: str, report_type: str) ->None:
+    """ TODO decription
+
+    Args:
+        #TODO
+
+    Returns:
+        None
+
+    """
+    #TODO implement me
+    # Requires
+    #number: url  :  filtered room count : street: house type: price
+    pass
