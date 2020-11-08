168bbce577608a963a6a11173ba0fecd6ca20e3a
Valentins Fedotovs <vtrader@inbox.lv>
2020-11-08 15:43:22 +0000

    Added test_gmail file and confirmed send test email works

---
+++ test_gmail.py
@@ -0,0 +1,17 @@
+import smtplib, ssl
+
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "info.wtads@gmail.com"  # Enter your address
+receiver_email = "info.wtads@gmail.com"  # Enter receiver address
+password = int("Please type password and press enter:")
+message = """\
+Subject: Hi there
+
+This message is sent from Python."""
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
+
