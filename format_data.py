c49fdc6cb9ec52e36b8109ace255ee065ecb5595
Valentins Fedotovs <vtrader@inbox.lv>
2020-11-08 19:29:40 +0000

    Implemented partially working oneline report

--- format_data.py
+++ format_data.py
@@ -1,5 +1,8 @@
 import re
 
+def main():
+    oneline_report('Ogre-report.txt', 'test.txt', 'type1')
+
 
 def oneline_report(source_file: str, dest_file: str, report_type: str) ->None:
     """ TODO decription
@@ -13,5 +16,34 @@ def oneline_report(source_file: str, dest_file: str, report_type: str) ->None:
     """
     #TODO implement me
     # Requires
-    #number: url  :  filtered room count : street: house type: price
-    pass
+    # number: url  :  filtered room count : street: house type: price
+    #
+    urls = []
+    room_counts = []
+    room_sizes = []
+    oneline_report = []
+    # Read data from file + RE
+    with open(source_file) as file_handle:
+        while True:
+            line = file_handle.readline()
+            match_url = re.search("https", line)
+            if match_url:
+                urls.append(line.rstrip('\n'))
+
+            match_room_count = re.search("Istabas:", line)
+            if match_room_count:
+                room_counts.append(line.rstrip('\n'))
+            match_room_size = re.search("Platība:", line)
+            if match_room_size:
+                room_sizes.append(line.rstrip('\n'))
+            if not line:
+                break
+
+    # print(urls)
+    # print(room_counts)
+    # print(room_sizes)
+    for i in range(len(urls) - 1):
+        oneline = urls[i] + " " + room_counts[i] + " " + room_sizes[i]
+        print(oneline)
+
+main()
