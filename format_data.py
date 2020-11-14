360c42511434eb9513b399644deeac28645ec726
Valentins Fedotovs <vtrader@inbox.lv>
2020-11-14 20:31:19 +0000

    Added street and price fields to oneline report

--- format_data.py
+++ format_data.py
@@ -21,6 +21,8 @@ def oneline_report(source_file: str, dest_file: str, report_type: str) ->None:
     urls = []
     room_counts = []
     room_sizes = []
+    room_streets = []
+    room_prices = []
     oneline_report = []
     # Read data from file + RE
     with open(source_file) as file_handle:
@@ -33,6 +35,15 @@ def oneline_report(source_file: str, dest_file: str, report_type: str) ->None:
             match_room_count = re.search("Istabas:", line)
             if match_room_count:
                 room_counts.append(line.rstrip('\n'))
+
+            match_room_street_count = re.search("Iela:", line)
+            if match_room_street_count:
+                room_streets.append(line.rstrip('\n'))
+
+            match_room_price = re.search("Price:", line)
+            if match_room_price:
+                room_prices.append(line.rstrip('\n'))
+
             match_room_size = re.search("Platība:", line)
             if match_room_size:
                 room_sizes.append(line.rstrip('\n'))
@@ -43,7 +54,7 @@ def oneline_report(source_file: str, dest_file: str, report_type: str) ->None:
     # print(room_counts)
     # print(room_sizes)
     for i in range(len(urls) - 1):
-        oneline = urls[i] + " " + room_counts[i] + " " + room_sizes[i]
+        oneline = urls[i] + " " + room_counts[i] + " " + room_sizes[i] + room_streets[i] + room_prices[i]
         print(oneline)
 
 main()
