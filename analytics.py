8521f796ced258fd14bcbe5986a88aae068b5365
Valentins Fedotovs <vtrader@inbox.lv>
2020-12-04 19:47:19 +0000

    Added df clean-up and initial analytics features

--- analytics.py
+++ analytics.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 """
 Main features of this module:
@@ -15,11 +15,154 @@ Detailed requirement list:
 
 """
 
-# Read Pandas df from CSV
 
-all_apartments = pd.read_csv("pandas-df.csv")
-#test_data.head()
+# loading data to dataframe from csv file
+all = pd.read_csv("pandas_df.csv")
 
-# Write df to csv file
-# df.to_csv("only-2-room.csv")
+# Excluding one cloumn
+no_url = all.loc[:, all.columns != 'URL']
+
+# data cleanup
+clean_room_col = no_url.replace(to_replace=r'Istabas:>', value='', regex=True)
+clean_sqm_col = clean_room_col.replace(to_replace=r'Platiba:>', value='', regex=True)
+clean_floor_col = clean_sqm_col.replace(to_replace=r'Stavs:>', value='', regex=True)
+clean_street_col = clean_floor_col.replace(to_replace=r'Iela:><b>', value='', regex=True)
+clean_price_col = clean_street_col.replace(to_replace=r'Price:>', value='', regex=True)
+
+# exclude Unamed column
+no_unamed = clean_price_col.loc[:, clean_price_col.columns != 'Unnamed: 0']
+
+### Sptitting columo value in to new columns by separator
+new = no_unamed["Size_sq_m"].str.split(" ", n = 1, expand = True)
+
+# making separate first name column from new data frame
+no_unamed["Size_sqm"]= new[0] # 0 - index at separation
+
+# drop old split column
+new = no_unamed.loc[:, no_unamed.columns != 'Size_sq_m']
+
+# cleanup lift column
+clean_lift_col = new.replace(to_replace=r'/lifts', value='', regex=True)
+
+### Sptitting price columo value in to new columns by separator
+new_price = clean_lift_col["Price"].str.split("(", n = 1, expand = True)
+
+# making separate first name column from new data frame
+clean_lift_col["Price_EUR"]= new_price[0]
+clean_lift_col["SQ_M_EUR"]= new_price[1]
+
+
+# remove EUR sign in price column and remove space
+no_euro_symb = clean_lift_col["Price_EUR"].str.split(" ", n = 2, expand = True)
+clean_lift_col["Price_in_eur"]= no_euro_symb[0] + no_euro_symb[1]
+
+# drop old split column
+clean_df_tmp = clean_lift_col.loc[:, clean_lift_col.columns != 'Price']
+clean_df = clean_df_tmp.loc[:, clean_df_tmp.columns != 'Price_EUR']
+
+
+# cleanup SQ_M_EUR - done
+# split at EUR simbol
+split_at_euro_symb = clean_df["SQ_M_EUR"].str.split("€", n = 1, expand = True)
+# create new column with from split df and use only 0 index
+clean_df["SQ_meter_price"]= split_at_euro_symb[0]
+
+
+# remve space from clumn value strings
+clean_df['SQ_meter_price'] = clean_df['SQ_meter_price'].str.replace(' ', '')
+
+# convert to float
+clean_df['SQ_meter_price'] = clean_df['SQ_meter_price'].astype(float)
+
+
+# exclude old SQ_M_EUR column == rady for save and export and analytics
+final_df = clean_df.loc[:, clean_df.columns != 'SQ_M_EUR']
+
+
+sorted_by_sqm = final_df.sort_values(by='SQ_meter_price', ascending=True)
+
+
+only_two_rooms = sorted_by_sqm[sorted_by_sqm['Room_count']=='2']
+#only_two_rooms
+
+#TODO:  add date incertion in data_formater feature for df (will calculate how many days old is ad)
+#TODO: convert othewhise plotting does not work
+# convert column values to int
+
+
+
+# convert str to int
+#clean_lift_col['Price_EUR'] = clean_lift_col['Price_EUR'].astype(int)
+#clean_lift_col["Price_EUR"].dtypes
+
+
+### Analytics Start Here ###
+
+# sorting df by column name
+sorted_by_sqm = clean_lift_col.sort_values(by='Size_sqm', ascending=True)
+
+all_room_prices = sorted_by_sqm['Price_EUR'].tolist() 
+all_room_price_count = len(all_room_prices)
+avg_all_price = sum(all_room_prices) / all_room_price_count
+print("All room apartment count: ",all_room_price_count )
+print("All room average price: ",avg_all_price )
+
+
+### Creating charts
+sorted_by_sqm.plot.scatter(x='Size_sqm',y="Price_EUR",s=100, title="All 1-4 room apartments",grid=True)
+#sorted_by_sqm.plot.scatter(x='Size_sqm',y="Room_count")
+#sorted_by_sqm.plot.scatter(x='Price_EUR',y="Room_count")
+
+
+
+### Filter out only 2 room apartments
+only_2_rooms = sorted_by_sqm[sorted_by_sqm['Room_count']=='2']
+
+# only_2_room_sqm = only_2_rooms.loc[:, only_2_rooms.columns == 'Size_sqm']
+
+
+
+# list of values of  column 
+two_room_prices = only_2_rooms['Price_EUR'].tolist() 
+two_room_price_count = len(two_room_prices)
+avg_price = sum(two_room_prices) / two_room_price_count
+
+
+
+
+### Creating charts
+only_2_rooms.plot.scatter(x='Size_sqm',y="Price_EUR",s=100, title="Only 2 room apartments",grid=True)
+
+## TODO add labels as variables
+print("2 room apartment count: ",two_room_price_count )
+print("2 room average price: ",avg_price )
+
+
+# Testing pychart
+only_2_rooms.groupby(['Size_sqm']).sum().plot(kind='pie',subplots=True,figsize=(7,7), autopct='%1.1f%%')
+
+
+
+### Filter out only 1 room apartments
+only_1_rooms = sorted_by_sqm[sorted_by_sqm['Room_count']=='1']
+
+one_room_prices = only_1_rooms['Price_EUR'].tolist() 
+one_room_price_count = len(one_room_prices)
+avg_one_room_price = sum(one_room_prices) / one_room_price_count
+
+print("1 room apartment count: ",one_room_price_count )
+print("1 room average price: ", avg_one_room_price )
+
+### Creating charts
+only_1_rooms.plot.scatter(x='Size_sqm',y="Price_EUR",s=100, title="Only 1 room apartments",grid=True)
+
+
+### Filter out only 3 room apartments
+only_3_rooms = sorted_by_sqm[sorted_by_sqm['Room_count']=='3']
+
+### Creating charts
+only_3_rooms.plot.scatter(x='Size_sqm',y="Price_EUR",s=100, title="Only 3 room apartments",grid=True)
+
+
+
 
