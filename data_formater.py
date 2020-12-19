d31e550d70a148f0b04545400d2197b9493c3c7c
Valentins Fedotovs <vtrader@inbox.lv>
2020-12-19 20:37:24 +0000

    Fixed PEP8 errors

--- data_formater.py
+++ data_formater.py
@@ -2,18 +2,19 @@ import re
 # import pandas as pd
 
 
-def write_mailer_report() ->None:
+def write_mailer_report() -> None:
     """ This function will use *raw-data-report.txt
         will format data in one line and will create Mailer_report.txt
         that should be used by gmailer module   """
 
-    text_data = create_oneline_report('Ogre-raw-data-report.txt', 'test.txt', 'type1')
+    text_data = create_oneline_report('Ogre-raw-data-report.txt')
     create_mailer_report(text_data, 'tmp_report.txt')
     create_filtered_report('tmp_report.txt', '1_rooms_tmp.txt', 'Istabas:>1')
     create_filtered_report('tmp_report.txt', '2_rooms_tmp.txt', 'Istabas:>2')
     create_filtered_report('tmp_report.txt', '3_rooms_tmp.txt', 'Istabas:>3')
     create_filtered_report('tmp_report.txt', '4_rooms_tmp.txt', 'Istabas:>4')
-    tmp_reports = ['1_rooms_tmp.txt', '2_rooms_tmp.txt', '3_rooms_tmp.txt', '4_rooms_tmp.txt']
+    tmp_reports = ['1_rooms_tmp.txt', '2_rooms_tmp.txt',
+                   '3_rooms_tmp.txt', '4_rooms_tmp.txt']
 
     room_count = 1
     for report in tmp_reports:
@@ -21,18 +22,12 @@ def write_mailer_report() ->None:
         room_count += 1
 
 
-def create_oneline_report(source_file: str, dest_file: str, report_type: str) ->None:
+def create_oneline_report(source_file: str) -> list:
     """ Converts raw-data-report to oneline report in memory
-
     Args:
         source_file: raw-data text file for example Ogre-raw-data-report.txt
-        dest_file: str : currently not implemented this is future feature for multiple report types
-        report_type: str: currently not imlemented
 
-    Returns:
-       
-       oneliner_report: str list
-
+    Returns: str list
     """
     urls = []
     room_counts = []
@@ -76,23 +71,26 @@ def create_oneline_report(source_file: str, dest_file: str, report_type: str) ->
                 break
     
     # Create pandas df for saving to csv
-    #  dict = {'URL': urls, 'Room_count': room_counts, 'Size_sq_m' : room_sizes, 
-    #            'Floor': room_floors, "Street": room_streets, 'Price': room_prices}
-
-    #df = pd.DataFrame(dict)
-
-    #df.to_csv("pandas_df.csv")
-
+    # dict = {'URL': urls, 'Room_count': room_counts,
+    #         'Size_sq_m' : room_sizes, 'Floor': room_floors,
+    #         'Street': room_streets, 'Price': room_prices}
+    # df = pd.DataFrame(dict)
+    # df.to_csv("pandas_df.csv")
 
     for i in range(len(urls) - 1):
-        # oneline = urls[i] + " " + room_counts[i] + " " + room_sizes[i] + " " + room_streets[i] + "   " + room_prices[i]
-        oneline = urls[i] + " " + room_counts[i] + " " + room_floors[i] + " " + room_sizes[i] + "   " + room_prices[i]  # this is workaround for SMTP module ASCI erorrs
+        # oneline = urls[i] + " " + room_counts[i] + " " + room_sizes[i] + " "
+        #           + room_streets[i] + "   " + room_prices[i]
+
+        # this is workaround for SMTP module ASCI errors
+        oneline = urls[i] + " " + room_counts[i] + " " + room_floors[i] \
+                  + " " + room_sizes[i] + "   " + room_prices[i]
+
         # print(oneline)
         oneline_report.append(oneline)
     return oneline_report
 
 
-def create_mailer_report(text: list,file_name: str) ->None:
+def create_mailer_report(text: list, file_name: str) -> None:
     """ Writes oneline data text to mailer report file """
 
     with open(file_name, 'a') as the_file:
@@ -100,7 +98,8 @@ def create_mailer_report(text: list,file_name: str) ->None:
             the_file.write(f"{line}\n")
 
 
-def create_filtered_report(source_file: str, dest_file: str, keyword: str) ->None:
+def create_filtered_report(source_file: str,
+                           dest_file: str, keyword: str) -> None:
     """ Function imitates bash grep for filtering text file
 
     """
@@ -111,7 +110,7 @@ def create_filtered_report(source_file: str, dest_file: str, keyword: str) ->Non
                     dfile.write(line)
 
 
-def merge_reports(source_file: str, dest_file: str, rc: int) ->None:
+def merge_reports(source_file: str, dest_file: str, rc: int) -> None:
     """ Function merges multiple tmp reports in final report """
 
     with open(dest_file, "a") as dfile:
@@ -122,6 +121,5 @@ def merge_reports(source_file: str, dest_file: str, rc: int) ->None:
             for line in sfile:
                 dfile.write(line)
             
-
 
 write_mailer_report()
