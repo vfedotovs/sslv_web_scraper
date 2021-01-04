6d4b27b38aa3b8c5127f11e3dacb5aa6975d0900
Valentins Fedotovs <vtrader@inbox.lv>
2021-01-04 19:33:28 +0000

    Refactoring and implemented iterate_reports function

--- analytics.py
+++ analytics.py
@@ -2,16 +2,16 @@ import pandas as pd
 
 
 # What needs to be done for analysis
-#0. load df from cleaned df - done 
+# Load df from cleaned df csv file 
 all_ads_df = pd.read_csv("cleaned-sorted-df.csv",index_col=False)
 
 
-#1. filter by room count 4 dfs  - crete 4 arrays - done
+# Split all apartment dataframe in 4 categories 
+# Create 4 dataFrames filtered by room count
 only_1_rooms = all_ads_df[all_ads_df['Room_count']==1]
 only_2_rooms = all_ads_df[all_ads_df['Room_count']==2]
 only_3_rooms = all_ads_df[all_ads_df['Room_count']==3]
 only_4_rooms = all_ads_df[all_ads_df['Room_count']==4]
-
 
 cat_list = [only_1_rooms, only_2_rooms, only_3_rooms, only_4_rooms, all_ads_df]
 
@@ -22,44 +22,42 @@ def mini_report(df_name):
     lines = []
     filter_col = 'Price_in_eur'
     price_list = df_name[filter_col].tolist() 
-    print("Debug info: ", price_list)
+    #print("Debug info: ", price_list)
     avg_price = sum(price_list) / len(price_list)
     min_price = min(price_list)
     max_price = max(price_list)
     price_range = max_price - min_price
-    
-    print("Advertisement count: ",  len(price_list))
+#    print("Advertisement count: ",  len(price_list))
+#    print(f'Average price: {avg_price} EUR')
+#    print(f'Min price: {min_price} EUR')
+#    print(f'Max price: {max_price} EUR')
+#    print(f'Price range: {price_range} ')
     str_ac = "Advertisement count: " + str(len(price_list))
-    lines.append(str_ac)
-    print(f'Average price: {avg_price} EUR')
     str_avp = "Average price: " + str(avg_price) + " EUR"
-    lines.append(str_avp)
-    print(f'Min price: {min_price} EUR')
     str_minp = "Min price: " + str(min_price) + " EUR"
-    lines.append(str_minp)
-    print(f'Max price: {max_price} EUR')
     str_maxp = "Max price: " + str(max_price) + " EUR"
+    str_pr = "Price range: " + str(price_range)  
+    lines.append(str_ac)
+    lines.append(str_avp)
+    lines.append(str_minp)
     lines.append(str_maxp)
-    print(f'Price range: {price_range} ')
+    lines.append(str_pr)
     return lines
-    
-    
-txt_reprots = []    
-for i, cat in enumerate(cat_list):
-    if i < 4:
-        print(f'--- Title: {i + 1} rooms for sale advertisements  ---')        
+
+def iterate_reports():
+    txt_reprots = []
+    for i, cat in enumerate(cat_list):
         cat_report = mini_report(cat)
         txt_reprots.append(cat_report)
+    print("----- debug info ----")
+    for i, reports in enumerate(txt_reprots):
+            print("")
+            print(str(i + 1) + " room apartment  price analysis")
+            for line in reports:
+                print(line)
 
-    else:
-        print(f'--- Title: all rooms for sale advertisements  ---')
-        cat_report = mini_report(cat)
-        txt_reprots.append(cat_report)
 
-print("----- debug info ----")
-for i, r in enumerate(txt_reprots):
-    print(" report number " , i + 1)
-    print(r)
+iterate_reports()
 
 
 # Category 1 advert price analisys
