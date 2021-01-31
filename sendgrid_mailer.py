021baa045d9998a8c16c9282a3996295c8b72345
Valentins Fedotovs <vtrader@inbox.lv>
2021-01-31 15:56:10 +0000

    Created sendgrid_mailer.py and updated requirements file

---
+++ sendgrid_mailer.py
@@ -0,0 +1,37 @@
+"""
+sendgrid_mailer is used for sending emails with pdf attachments
+"""
+import base64
+import os
+
+
+from sendgrid.helpers.mail import (
+    Mail, Attachment, FileContent, FileName,
+    FileType, Disposition, ContentId)
+from sendgrid import SendGridAPIClient
+
+message = Mail(
+    from_email=(os.environ.get('SRC_EMAIL')),
+    to_emails=(os.environ.get('DEST_EMAIL'),
+    subject='Sending with Twilio SendGrid is Fun',
+    html_content='<strong>and easy to do anywhere, even with Python</strong>')
+file_path = 'example.pdf'
+with open(file_path, 'rb') as f:
+    data = f.read()
+    f.close()
+encoded = base64.b64encode(data).decode()
+attachment = Attachment()
+attachment.file_content = FileContent(encoded)
+attachment.file_type = FileType('application/pdf')
+attachment.file_name = FileName('test_filename.pdf')
+attachment.disposition = Disposition('attachment')
+attachment.content_id = ContentId('Example Content ID')
+message.attachment = attachment
+try:
+    sendgrid_client = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+    response = sendgrid_client.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+except Exception as e:
+    print(e.message)
