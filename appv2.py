ba2c673ab2ac58911cbeca4494acfd057b513e7e
Valentins Fedotovs <vtrader@inbox.lv>
2021-01-31 18:52:54 +0000

    Created appv2.py and renamed report_creator to pdf_creator

---
+++ appv2.py
@@ -0,0 +1,17 @@
+import web_scraper
+import df_cleaner
+import analytics
+import pdf_creator
+import sendgrid_mailer
+
+
+def main():
+    web_scraper
+    df_cleaner
+    analytics
+    pdf_creator
+    sendgrid_mailer
+
+
+if __name__ == "__main__":
+    main()
