5bba8f0373f2f77b256bdaafc11bca660fb63627
Valentins Fedotovs <vtrader@inbox.lv>
2021-04-06 16:15:27 +0100

    Updated app.py with milestone 2 version

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
+#!/usr/bin/env python3
+"""
+appv2 is 2.0 release of ss.lv web scraper
+New features:
+    - Extract and save raw data as txt file for specific city apartments for sale advert information
+    - Extracts 2 new attributes of advert - date inserted advert and view count is 1 (known issue)
+    - Clean and save data as python data frame in csv format
+    - Calculate basic price statistics and save them to txt file
+    - Use pdf_creator to generate chrats from data frame and create report in pdf file format
+    - Use Sendgrid API as email sender with capability to attach pdf files
+"""
 import web_scraper
 import data_formater
-import gmailer
+import df_cleaner
+import analytics
+import pdf_creator
+import sendgrid_mailer
 
 
 def main():
     web_scraper
     data_formater
-    gmailer
+    df_cleaner
+    analytics
+    pdf_creator
+    sendgrid_mailer
 
 
 if __name__ == "__main__":
