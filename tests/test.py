fd3723947be55031b87224adb215ed06e79a0a90
Valentins Fedotovs <vtrader@inbox.lv>
2021-05-01 15:17:35 +0100

    Added data and tests folder with somple files

---
+++ tests/test.py
@@ -0,0 +1,17 @@
+# this is example of unittest code
+import unittest
+
+#TODO: implement module testing 
+from x import function_name
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+if __name__ == '__main__':
+    unittest.main()
