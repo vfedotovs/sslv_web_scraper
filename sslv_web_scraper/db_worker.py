99f5e9e38cc6baee9215ad9a49bf48d14fc5911f
Valentins Fedotovs <39173395+vfedotovs@users.noreply.github.com>
2021-06-28 00:15:08 +0100

    Implemented functionality (#11)

--- sslv_web_scraper/db_worker.py
+++ sslv_web_scraper/db_worker.py
@@ -7,9 +7,9 @@ tracking how many days they stay in listed state, move ads to delisted_ads
 table after ads are removed from website and reporting capability.
 
 Todo functionality:
-    1.[] Load daily csv data to data frame
-    2.[] Extract ad hashes from data frame
-    3.[] Extract ad hashes from database listed_ads table
+    1.[x] Load daily csv data to data frame
+    2.[x] Extract ad hashes from data frame
+    3.[x] Extract ad hashes from database listed_ads table
     4.[] Categorize hases in 3 categories
         - new hashes (for insert to listed_ads table)
         - seen hashes but not delisted yet (increment listed days value)
@@ -35,6 +35,9 @@ def db_worker_main() -> None:
     print("DEBUG: Loaded cleaned-sorted-df.csv to dataframe in memory ...")
     df_hashes = get_data_frame_hashes('cleaned-sorted-df.csv')
     print("DEBUG: calculated hashes from data fame URLs ...")
+    tuple_listed_hashes = get_hashes_from_table()
+    string_listed_hashes = clean_db_hashes(tuple_listed_hashes)
+    print("DEBUG: Extracted from table listed_ads URL hashes  ...")
 
 
 def get_data_frame_hashes(df_filename: str) -> list:
@@ -49,12 +52,46 @@ def get_data_frame_hashes(df_filename: str) -> list:
 
 
 def extract_hash(full_url: str) -> str:
-    """ Exctracts 5 caracter hash from URL link and returns hash"""
+    """Exctracts 5 caracter hash from URL link and returns hash"""
     chunks = full_url.split("/", 9)
     last_chunk = chunks[9]
     split_chunk = last_chunk.split(".")
     url_hash = split_chunk[0]
     return url_hash
+
+
+def get_hashes_from_table() -> None:
+    """Iterate over all records listed_ads table and return list of hashes"""
+    conn = None
+    listed_db_hashes = []
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        cur.execute("SELECT url_hash FROM listed_ads ORDER BY url_hash")
+        row = cur.fetchone()
+        while row is not None:
+            listed_db_hashes.append(row)
+            row = cur.fetchone()
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+    return listed_db_hashes
+
+
+def clean_db_hashes(hash_list: list) -> list:
+    """Removes unnecesary characters from hashe list and returns only
+    string list with clean hashes"""
+    clean_hashes = []
+    for element in hash_list:
+        str_element =  ''.join(element)
+        clean_element = str_element.replace("'", "").replace(")", "")
+        clean_hash = clean_element.replace("(", "").replace(",", "")
+        clean_hashes.append(clean_hash)
+    return clean_hashes
 
 
 db_worker_main()
