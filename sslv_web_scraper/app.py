971c0da310c0b3c59a6df7dcad6e0936c22d772d
Valentins Fedotovs <39173395+vfedotovs@users.noreply.github.com>
2021-08-10 16:52:19 +0100

    Removed outdated module description in app.py

--- sslv_web_scraper/app.py
+++ sslv_web_scraper/app.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python3
 """
-app.py  is 1.2.0 release of ss.lv web scraper
-
-New release features:
-    - Extract and save raw data as txt file for specific city apartments for sale advert information
-    - Extracts 2 new attributes of advert - date inserted advert and view count is 1 (known issue)
-    - Clean and save data as python data frame in csv format
-    - Calculate basic price statistics and save them to txt file
-    - Use pdf_creator to generate chrats from data frame and create report in pdf file format
-    - Use Sendgrid API as email sender with capability to attach pdf files
+This main module of ss.lv web scraper app that calls all submodules
+For email report functionality uncoment sendgrid_mailer import and
+include it to main()
 """
 import web_scraper
 import data_formater
