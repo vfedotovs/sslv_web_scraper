f5d840bac9a42c4ad37292e7b9b83fac2e4c0136
Valentins Fedotovs <39173395+vfedotovs@users.noreply.github.com>
2021-10-02 17:07:00 +0100

    Changed app/main.py new background task is called from ws_worker.py ws_worker_main

--- app/main.py
+++ app/main.py
@@ -2,9 +2,8 @@ import uvicorn
 from fastapi import BackgroundTasks, FastAPI
 
 # Example how to import my modules
-# Folder app > folder wsmodules > web_scraper.py function scrape_website() 
-from app.wsmodules.web_scraper import scrape_website
-
+# Folder app > folder wsmodules > web_scraper.py function scrape_website()
+from app.wsmodules.ws_worker import ws_worker_main
 
 app = FastAPI()
 
@@ -16,7 +15,7 @@ def home():
 
 @app.get("/run-task/{city}")
 async def run_long_task(city: str, background_tasks: BackgroundTasks):
-    background_tasks.add_task(scrape_website)
+    background_tasks.add_task(ws_worker_main)
     return {"message": "run task for ogre  city in the background"}
 
 
@@ -24,4 +23,3 @@ if __name__ =='__main__':
     uvicorn.run(app)
 
 
-#https://fastapi.tiangolo.com/tutorial/background-tasks/
