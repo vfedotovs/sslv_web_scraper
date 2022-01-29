f38c228294ed7ce8aec47d52734ea285175672be
Valentins Fedotovs <39173395+vfedotovs@users.noreply.github.com>
2022-01-29 16:39:23 +0000

    Fixed issue incorrect email body formatting #80

--- app/wsmodules/sendgrid_mailer.py
+++ app/wsmodules/sendgrid_mailer.py
@@ -31,27 +31,28 @@ def remove_tmp_files() -> None:
             print(f'Error: {data_file} : {e.strerror}')
 
 
-
 def sendgrid_mailer_main() -> None:
     """Main module entry point"""
-    print("Debug info: Starting sendgrid mailer module ... ")
-    # Read text file for including plain text as email content
+    print("Debug info: Starting sendgrid mailer module ...")
     with open('email_body_txt_m4.txt') as f:
-        content = f.readlines()
-    str_text = '\n'.join([i for i in content[1:]])
+        file_content = f.readlines()
+    email_body_content = ''.join([i for i in file_content[1:]])
+
     # Creates Mail object instance
-    message = Mail(from_email=(os.environ.get('SRC_EMAIL')),
-                   to_emails=(os.environ.get('DEST_EMAIL')),
-                   subject='Ogre City Apartments for sale from ss.lv webscraper v1.4.3',
-                   plain_text_content=str_text)
+    message = Mail(
+            from_email=(os.environ.get('SRC_EMAIL')),
+            to_emails=(os.environ.get('DEST_EMAIL')),
+            subject='Ogre Apartments for sale from ss.lv webscraper v1.4.3',
+            plain_text_content=email_body_content)
     try:
         sendgrid_client = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
         response = sendgrid_client.send(message)
         print("Email sent response code:", response.status_code)
     except Exception as e:
         print(e.message)
-    print("Debug info: Completed website parsing module ... ")
+    print("Debug info: Removing temp files ... ")
     remove_tmp_files()
+    print("Debug info: Completed sendgrid mailer module... ")
 
 
 # Read pdf file file for  attachment
