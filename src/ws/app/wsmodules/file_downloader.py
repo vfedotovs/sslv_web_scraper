dd78d780b534e8b58e98e0b403b6a89280758474
Valentins Fedotovs <39173395+vfedotovs@users.noreply.github.com>
2023-02-20 23:13:05 +0000

    Change os.environ to os.environ.get

--- src/ws/app/wsmodules/file_downloader.py
+++ src/ws/app/wsmodules/file_downloader.py
@@ -7,8 +7,8 @@ import boto3
 
 # S3_BUCKET_NAME = os.environ['S3_BUCKET']
 S3_LAMBDA_BUCKET_NAME="lambda-ogre-scraped-data"
-AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 s3 = boto3.client('s3', region_name="eu-west-1",
                   aws_access_key_id=AWS_ACCESS_KEY_ID,
                   aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
