63c0ea74f7eeedec940e7ae9cfd47dcc27887976
Valentins Fedotovs <39173395+vfedotovs@users.noreply.github.com>
2023-03-19 01:39:54 +0000

    Add missing imports in db_worker and df_cleaner

--- tests/test_04_module_db_worker.py
+++ tests/test_04_module_db_worker.py
@@ -1,13 +1,15 @@
-
-#from src.ws.app.wsmodules.db_worker import extract_hash
+from src.ws.app.wsmodules.db_worker import extract_hash
+import psycopg2
 
 
 def test_extract_hash():
     # Test with a URL that contains a hash
-    assert extract_hash("https://ss.lv/msg/lv/real-estate/flats/riga/centre/p0q31691478.html") == "31691478"
+    assert extract_hash(
+        "https://ss.lv/msg/lv/real-estate/flats/riga/centre/p0q31691478.html") == "31691478"
 
     # Test with a URL that does not contain a hash
-    assert extract_hash("https://ss.lv/msg/lv/real-estate/flats/riga/centre/") == ""
+    assert extract_hash(
+        "https://ss.lv/msg/lv/real-estate/flats/riga/centre/") == ""
 
     # Test with an empty string
     assert extract_hash("") == ""
