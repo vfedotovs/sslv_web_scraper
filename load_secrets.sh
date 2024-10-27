504fb08a415f7119fa1e2ed3fff66d117b7c4a88
Valentins Fedotovs <39173395+vfedotovs@users.noreply.github.com>
2024-10-27 16:47:12 +0000

    (cicd) Add load_secrets.sh file

---
+++ load_secrets.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Download env files
+aws s3 cp s3://sslv-ws-m5-cicd-files/.env.prod .
+aws s3 cp s3://sslv-ws-m5-cicd-files/database.ini .
+
+# Fetch the secret from Secrets Manager
+secret=$(aws secretsmanager get-secret-value --secret-id sslv_creds --query SecretString --output text)
+
+# Parse the JSON to get individual values
+export S3_BUCKET=$(echo $secret | jq -r '.s3_db_backups')
+export SENDGRID_API_KEY=$(echo $secret | jq -r '.sendgrid_api')
+export DEST_EMAIL=$(echo $secret | jq -r '.dest_email')
+export SRC_EMAIL=$(echo $secret | jq -r '.src_email')
